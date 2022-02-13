93dd8ccaf4c161da6689ac7a15aeb2a95d20bbfa
Pawel Bialek <pawel.bialek@e-file.pl>
2022-02-13 15:00:30 +0100

    heroku deployment

--- config/prod.js
+++ config/prod.js
@@ -5,5 +5,11 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-    stripeSecretKey: process.env.STRIPE_SECRET_KEY
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+    JWT_SECRET: process.env.JWT_SECRET,
+    REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY,
+    SESSION_EXPIRY: process.env.SESSION_EXPIRY,
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+    COOKIE_SECRET: process.env.COOKIE_SECRET,
+    WHITELISTED_DOMAINS: process.env.WHITELISTED_DOMAINS
 };
